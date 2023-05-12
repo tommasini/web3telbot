e2fdfe5bf7fe25d2709cef752e7cb32be061c488
Andre Ribeiro <andresmribeiro@gmail.com>
2023-05-13 00:13:36 +0100

    bot connected to metamask sdk

--- bot/bot.ts
+++ bot/bot.ts
@@ -1,8 +1,21 @@
 import {Bot, InlineKeyboard} from "grammy";
+import {MetaMaskSDK} from "@metamask/sdk"
 import dotenv from "dotenv";
 
 dotenv.config();
 
+// Metamask config
+const sdk = new MetaMaskSDK({
+    shouldShimWeb3: false,
+});
+
+const ethereum = sdk.getProvider();
+ethereum?.request({
+    method: "eth_requestAccounts",
+    params: [],
+});
+
+// Bot config
 // maps the chat id to a map of <user id to wallet>
 const chats = new Map<number, Map<number, string>>();
 
@@ -12,12 +25,34 @@ const bot = new Bot(process.env.BOT_API_KEY!!);
 bot.command("addwallet", async (ctx) => {
     // get the chat id and store the user's information plus its wallet to the current chat.
     // we should also allow the user to add their wallet to the overall bot database if requested.
+    const link = sdk.getUniversalLink();
     const wallet = ctx.message?.text?.split(" ")?.[1];
     if (wallet) {
         const chat_id = (await ctx.getChat()).id;
         const item = (chats.has(chat_id) ? chats : chats.set(chat_id, new Map())).get(chat_id);
         item?.set(ctx.from!!.id, wallet);
     }
+
+    await ctx.reply(link);
+})
+
+bot.command("send", async (ctx) => {
+    const chat_id = (await ctx.getChat()).id;
+    const toAccount = ctx.message?.text?.split(" ")?.[1];
+    const amount = ctx.message?.text?.split(" ")?.[2]; // how to use?
+    await ethereum?.request({
+        method: "eth_sendTransaction",
+        params: [
+            {
+                from: chats.get(chat_id)?.get(ctx.from!!.id),
+                to: toAccount, //"0xC4955C0d639D99699Bfd7Ec54d9FaFEe40e4D272"
+                value: "0x38D7EA4C68000", // Only required to send ether to the recipient from the initiating external account.
+                // gasPrice: "0x09184e72a000", // Customizable by the user during MetaMask confirmation.
+                //gas: "0x2710", // Customizable by the user during MetaMask confirmation.
+            },
+        ],
+    })
+
 })
 
 
